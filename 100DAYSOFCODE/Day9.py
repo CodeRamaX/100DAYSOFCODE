976a89689a8f885323d85fc0094138be28b1a3b0
Rama Shankar Singh <ramashankarsingh2811@gmail.com>
2024-12-29 02:43:27 +0530

    Day9.py

--- 100DAYSOFCODE/Day9.py
+++ 100DAYSOFCODE/Day9.py
@@ -1,16 +1,43 @@
 import requests
 
-api_key = '18801b3d598d93968d727c48ca03c6d7'
+api_key='18801b3d598d93968d727c48ca03c6d7'
 
-user_input = input("Enter city: ")
+user_input=input("Enter City: ")
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+weather_api=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
 
-if weather_data.json()['cod'] == '404':
+if weather_api.json()['cod'] == '404':
     print("No City Found")
 else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
+    weather=weather_api.json()['weather'][0]['main']
+    temp=weather_api.json()['main']['temp']
+    humidity=weather_api.json()['main']['humidity']
+    pressure=weather_api.json()['main']["pressure"]
 
-    print(f"The weather in {user_input} is: {weather}")
-    print(f"The temperature in {user_input} is: {temp}°C")
+    print(f"The Weather in {user_input} is: {weather}")
+
+    print(f"The Temperature in {user_input} is: {temp}°C")
+    
+    print(f"The Humidity in {user_input} is: {humidity}%")
+
+    print(f"The Pressure in {user_input} is: {pressure} hPa")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
