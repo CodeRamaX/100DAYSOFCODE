71120224550af984db3d3c4093a175f5729b170a
Rama Shankar Singh <ramashankarsingh2811@gmail.com>
2025-01-22 02:03:51 +0530

    Add files via upload

---
+++ 100DAYSOFCODE/Day32/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter two num: ");
+//     scanf("%d %d",&a,&b);
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+//     printf("%d and %d ",a,b);
+// }
