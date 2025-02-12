4b4edfe3734c13118585ebfca6cc8a94afc64c11
Rama Shankar Singh <ramashankarsingh2811@gmail.com>
2025-02-12 23:12:59 +0530

    Add files via upload

---
+++ 100DAYSOFCODE/Day50/day50.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (num >= 10) {
+        sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    printf("Digital Root: %d\n", num);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
