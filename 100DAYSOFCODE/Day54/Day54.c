36a6a9d605d83c29f8e9c6e263038d667a915cab
Rama Shankar Singh <ramashankarsingh2811@gmail.com>
2025-02-18 20:04:01 +0530

    Add files via upload

---
+++ 100DAYSOFCODE/Day54/Day54.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char s[100]; 
+   printf("Enter a string:");
+   gets(s); 
+   strrev(s);
+   printf("The reverse of the string: %s\n", s);
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
